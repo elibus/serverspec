c7d7413710df3af76bb9adf5fbaf76571ae21e2b
Marco Tizzoni <marco.tizzoni@gmail.com>
2015-09-13 21:43:45 +0200

    MX and NS type test

---
+++ lib/serverspec/type/nameserver_zone.rb
@@ -0,0 +1,18 @@
+module Serverspec::Type
+  class NameserverZone < Base
+
+    def mx
+      ret = @runner.run_command("dig +search +short +time=1 -q #{escape(@name)} a @127.0.0.1")
+      ret.stdout.strip
+    end
+
+    def ns
+      ret = @runner.run_command("dig +search +short +time=1 -q #{escape(@name)} ns @127.0.0.1")
+      ret.stdout.strip
+    end
+
+    def has_record?(type, query, result)
+      #inspection['Volumes'][container_path] == host_path
+    end
+  end
+end
